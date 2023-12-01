713b133bdc5b689853af76dde42b18ed60dd50d0
Samolazoff <samolazov.roman.alex@gmail.com>
2023-12-01 14:05:21 +0300

    ADD panel lang

--- client/src/components/LangPanel/LangPanel.tsx
+++ client/src/components/LangPanel/LangPanel.tsx
@@ -1,16 +1,18 @@
 import {useAppDispatch } from '../../store/hook';
 import { changeLangEn, changeLangRu } from '../../store/slices/langSlice';
 
+import './LangPanel.sass';
+
 const LangPanel = () => {
     const dispatch = useAppDispatch();
     return (
         <div className="lang-panel">
-            <ul>
-                <li>
-                    <button id='btn-RU' onClick={()=> dispatch(changeLangRu())}>Рус</button>
+            <ul className='lang-panel-items'>
+                <li className='lang-panel_item'>
+                    <button id='btn-RU' className='btn-lang' onClick={()=> dispatch(changeLangRu())}>Рус</button>
                 </li>
                 <li>
-                    <button id='btn-EN' onClick={()=> dispatch(changeLangEn())}>Eng</button>
+                    <button id='btn-EN' className='btn-lang' onClick={()=> dispatch(changeLangEn())}>Eng</button>
                 </li>
             </ul>
         </div>
