4cdf2eccf184c09ac137a30e4903f2ce18b44764
unknown <samolazov.roman.alex@gmail.com>
2023-12-12 16:50:45 +0300

    ADD contacts block

--- client/src/components/FormContacts/FormContacts.tsx
+++ client/src/components/FormContacts/FormContacts.tsx
@@ -2,9 +2,31 @@ import './FormContacts.sass';
 
 const FormContacts = () => {
     return (
-        <div>
-            
-        </div>
+        <form className="app-contacts-form-feedback" method="post">
+            <h3 className="app-contacts-form-feedback__title">Мы на связи</h3>
+            <h4 className="app-contacts-form-feedback__subtitle">Оставте заявку, и мы свяжемся с вами</h4>
+            <div className="app-contacts-form-feedback-wrap">
+                <div className="app-contacts-form-feedback-content">
+                    <div className="app-contacts-form-feedback-wrap-input">
+                        <label className='app-contacts-form-feedback-input'>
+                            Ваше имя:
+                            <input id="formFeedbackName" type="text" name="formFeedbackName"/>
+                        </label >
+                        <label className='app-contacts-form-feedback-input'>
+                            Название компании:
+                            <input id="formFeedbackCompany" type="text" name="formFeedbackCompany"/>
+                        </label>
+                        <label className='app-contacts-form-feedback-input'>
+                            Номер телефона:
+                            <input id="formFeedbackPhone" type="tel" name="formFeedbackPhone"/>
+                        </label>
+                    </div>
+                    <textarea id="formFeedbackPost" name="formFeedbackPost"> </textarea>
+                </div>
+                <span className="text-confid">Нажимая кнопку "Отправить", вы соглашаетесь на обработку персональных данных в соответствие с Политикой конфидициальности.</span>
+                <button className="btn" type="submit">Отправить</button>
+            </div>
+        </form>
     );
 };
 
